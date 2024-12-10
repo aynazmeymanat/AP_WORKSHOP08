7edb4316b16243c322984dfb681be4b5263e8bc6
aynazmeymanat <aynazmeymanat@gmail.com>
2024-12-10 12:09:35 +0330

    fields and constructor added

--- src/Note.java
+++ src/Note.java
@@ -1,2 +1,13 @@
+import java.util.Date;
+
 public class Note {
+    private String name;
+    private String content;
+    private Date creationDate;
+
+    public Note(String name, String content) {
+        this.name = name;
+        this.content = content;
+        this.creationDate = new Date();
+    }
 }
